87ec99380622ab8b024430a9ab4a7cc1627ab8a6
JanneHamalainen <janne270685@gmail.com>
2015-09-21 17:37:38 +0300

    joojotellu :D

--- Kerta02/main.cpp
+++ Kerta02/main.cpp
@@ -1,6 +1,10 @@
 #include "Person.h"
 #include "Player.h"
-
+#include "Person.h"
+#include <iostream>
 int main(int argc, char** argv) {
+	Person personA("Matti Nykänen", 50);
+	Person personB("Johanna Tukiainen", 100);
+	Person personC("Henna Kalinainen", 75);
 	return 0;
 }
